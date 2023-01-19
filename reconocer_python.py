334f2800580e38e2bdc9ea6afbe7186ff66b10a5
Dorian <adonosoa@hotmail.com>
2023-01-19 15:14:46 -0300

    first commit

---
+++ reconocer_python.py
@@ -0,0 +1,93 @@
+#declaración de variables nro entero
+num1 = 42
+#declaración de variable nro float 
+num2 = 2.3
+#declaración de variable boleano data type 
+boolean = True
+#declaración de variables dara type 
+string = 'Hello World'
+# arreglos 
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
+fruit = ('blueberry', 'strawberry', 'banana')
+#impresion de datos
+print(type(fruit))
+# impresion de datos con arreglo
+print(pizza_toppings[1])
+# diccionario
+pizza_toppings.append('Mushrooms')
+# impresion de datos con arreglo
+print(person['name'])
+person['name'] = 'George'
+person['eye_color'] = 'blue'
+print(fruit[2])
+# Condicionales 
+if num1 > 45:
+    print("It's greater")
+else:
+    print("It's lower")
+
+if len(string) < 5:
+    print("It's a short word!")
+elif len(string) > 15:
+    print("It's a long word!")
+else:
+    print("Just right!")
+
+for x in range(5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+while(x < 5):
+    print(x)
+    x += 1
+
+pizza_toppings.pop()
+pizza_toppings.pop(1)
+
+print(person)
+person.pop('eye_color')
+print(person)
+# conficionales
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':
+        break
+
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section
+"""
+
+# print(num3)
+# num3 = 72
+# fruit[0] = 'cranberry'
+# print(person['favorite_team'])
+# print(pizza_toppings[7])
+#   print(boolean)
+# fruit.append('raspberry')
+# fruit.pop(1)
